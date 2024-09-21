2e4be669c761cb35f05e360d9a16a17a5aec6725
Shahir Ahmed <ahmeds@dickinson.edu>
2024-09-21 11:28:03 -0400

    adds a basic header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "../styles/Header.css";
+import icon from "../assets/icon.svg";
+
+function Header() {
+	const [activePage, setActivePage] = useState("Dashboard");
+
+	// Function to handle page changes and set the active page
+	const handlePageChange = (page) => {
+		setActivePage(page);
+	};
+
+	const handleTitleClick = () => {
+		window.location.reload();
+	};
+
+	return (
+		<div className="header">
+			<div className="title" onClick={handleTitleClick}>
+				<img src={icon} alt="Sweet Friend's Icon" />
+				<h1>SWeet Friend</h1>
+			</div>
+			<div className="links">
+				<a
+					href="#dashboard"
+					className={activePage === "Dashboard" ? "active" : ""}
+					onClick={() => handlePageChange("Dashboard")}
+				>
+					Dashboard
+				</a>
+				<a
+					href="#logs"
+					className={activePage === "Logs" ? "active" : ""}
+					onClick={() => handlePageChange("Logs")}
+				>
+					Logs
+				</a>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
