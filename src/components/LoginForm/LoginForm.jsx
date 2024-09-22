9da38a7b7680e0fbad1a95aad131b40be359b1f7
Dennis <dennis.miczek@gmail.com>
2024-09-21 23:22:40 -0400

    merge css changes from login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -9,27 +9,29 @@ const LoginForm = () => {
         navigate("/home");
     }
   return (
-    <div className='wrapper'>
-        <form action=''>
-            <h1>Login</h1>
-            <div className="input-box">
-                <input type="text" placeholder="Username" required />
-                
-            </div>
-            <div className="input-box">
-                <input type="password" placeholder="Password" required/>
-                
-            </div>
-            <div className="remember-forgot">
-                <label><input type="checkbox" />Remember me</label>
-                <a href="#">Forgot password</a>
-            </div>
-            <button type="submit" onClick={handleClick}>Login</button>
-            <div className="register-link">
-                <p>Don't have an account? <a href="#">Register</a></p>
-            </div>
-        </form>
+    <div className='dialog'>
+        <div className='wrapper'>
+            <form action=''>
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Username" required />
+                    
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required/>
+                    
+                </div>
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Remember me</label>
+                    <a href="#">Forgot password</a>
+                </div>
+                <button type="submit" onClick={handleClick}>Login</button>
+                <div className="register-link">
+                    <p>Don't have an account? <a href="#">Register</a></p>
+                </div>
+            </form>
 
+        </div>
     </div>
   )
 }
