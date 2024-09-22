2fa62b0a411ef71f8ebeb556a3167e4e8eacb850
WillingLearn <34353944+WillingLearn@users.noreply.github.com>
2024-09-21 23:04:48 -0400

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Header from "./components/Header";
 import Dashboard from "./components/Dashboard";
 import "./App.css";
-
+import LoginForm from "./components/LoginForm/LoginForm";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import "./index.css";
 function App() {
 	return (
 		<>
-			<Header />
-			<Dashboard />
+			<Routes>
+				<Route path="/" element={<LoginForm/>} />
+				<Route path="/home" element={<Home/>} />
+			</Routes>
+
+			{/* <Header />
+			<Dashboard /> */}
+			{/* <LoginForm/> */}
 		</>
 	);
 }
